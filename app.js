a2b6b7c5a597ee686b80de348364adeafa6fab5a
Vivek Naskar <viveknaskar2912@gmail.com>
2018-04-01 22:08:30 +0530

    Creating a sever

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-var count = require('./counter');
+var http = require('http');
 
-console.log(count(['Vivek', 'Lekshmi', 'Bruce', 'Tony']));
+var server = http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type':'text/plain'});
+    res.end('Hey there, this is the nodejs first server');
+});
+
+server.listen(3000, '127.0.0.1');
+console.log('The application is listening to port 3000');
