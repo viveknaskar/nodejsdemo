8b2261fbc5aaf89506dda533d2410e753b14c383
Vivek Naskar <viveknaskar2912@gmail.com>
2018-04-01 17:25:28 +0530

    Writing file using fs  after reading from a file.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 
 var readfile = fs.readFileSync('readMe.txt', 'utf8');
+//readFileSync method reads the file syncronously meaning it wont go to other code/lines until the method is fully executed.
 
-console.log(readfile);
+fs.writeFileSync('writeMe.txt', readfile);
