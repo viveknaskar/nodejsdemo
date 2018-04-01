2589729554a75239058756da673131041a6e03c9
Vivek Naskar <viveknaskar2912@gmail.com>
2018-04-01 16:40:47 +0530

    Event emitter

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-var stuff = require('./stuff');
+var events = require('events');
+var util = require('util');
 
-console.log(stuff.count(['Vivek', 'Lekshmi', 'Bruce', 'Tony']));
-console.log("The value of pi : " + stuff.pi);
+var Person = function(name){
+    this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var vivek = new Person('vivek');
+var stark = new Person('stark');
+var wayne = new Person('wayne');
+var people = [vivek, stark, wayne];
+
+people.forEach(function(person){
+    person.on('speak', function(msg){
+        console.log(person.name + ' said: ' + msg);
+    });
+});
+
+vivek.emit('speak', 'hey dudes!');
+stark.emit('speak', 'lets go party!');
+wayne.emit('speak', 'sounds like a plan to me!')
+vivek.emit('speak', 'thats awesome!');
+stark.emit('speak', 'we will go in my car!');
+wayne.emit('speak', 'sure dude!');
+
+
