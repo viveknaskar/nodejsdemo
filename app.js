4e774159a94560cd978b9d2e8a2b08111453fc73
Vivek Naskar <viveknaskar2912@gmail.com>
2018-04-01 22:22:02 +0530

    consoling request url

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
 var server = http.createServer(function(req, res){
+    console.log('The request was made from ' + req.url);
     res.writeHead(200, {'Content-Type':'text/plain'});
     res.end('Hey there, this is the nodejs first server');
 });
