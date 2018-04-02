4e008af65887f9cfe2d123665574570f15abb488
Vivek Naskar <viveknaskar2912@gmail.com>
2018-04-03 00:50:52 +0530

    serving JSON to the client

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var http = require('http');
 var fs = require('fs');
 
-// a pipe is an elegant way of reading data from stream and writing it.
-// pipes only works on readable streams
-
 var server = http.createServer(function(req, res){
     console.log('The request was made from ' + req.url);
-    res.writeHead(200, {'Content-Type':'text/html'});
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-    myReadStream.pipe(res);
+    res.writeHead(200, {'Content-Type':'application/json'});
+    var myObj = {
+        name: 'Vivek',
+        job: 'Developer',
+        country: 'India'
+    };
+    res.end(JSON.stringify(myObj)); /*here we can pass only string or buffer, so we are making the JSON as string */
+   
 });
 
 server.listen(3000, '127.0.0.1');
