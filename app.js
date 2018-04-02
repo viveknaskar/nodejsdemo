e3929bd4c24c610a553b4d322adfb8eb0b5d22e9
Vivek Naskar <viveknaskar2912@gmail.com>
2018-04-03 00:38:12 +0530

    serving HTML to the client

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
     console.log('The request was made from ' + req.url);
-    res.writeHead(200, {'Content-Type':'text/plain'});
-    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+    res.writeHead(200, {'Content-Type':'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
     myReadStream.pipe(res);
 });
 
