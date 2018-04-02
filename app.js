a53833d19ea63d092ad3dcf550351c60aac6cd98
Vivek Naskar <viveknaskar2912@gmail.com>
2018-04-02 22:28:08 +0530

    reading data as chunks from an ext file as sending it as a response using pipe

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/writeMe.txt');
+// a pipe is an elegant way of reading data from stream and writing it.
+// pipes only works on readable streams
 
-myReadStream.on('data', function(chunk){
-    console.log('new chunk received:');
-    myWriteStream.write(chunk);
-});
-
-
-/* var server = http.createServer(function(req, res){
+var server = http.createServer(function(req, res){
     console.log('The request was made from ' + req.url);
     res.writeHead(200, {'Content-Type':'text/plain'});
-    res.end('Hey there, this is the nodejs first server');
+    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+    myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('The application is listening to port 3000');
- */
